6b6812dc9d1b1083a75d4f28ca867c901a63ae54
troudi-malek <malek.troudi@esprit.tn>
2026-09-07 16:06:57 +0100

    deploy fix

--- src/services/authenticationService.ts
+++ src/services/authenticationService.ts
@@ -1,28 +1,35 @@
 import axios from "axios";
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const Login = async (email: string, password: string) => {
     try {
-        console.log(`${API_URL}admin/login`, email, password)
-        const response = await axios.post(`${API_URL}admin/login`, { email, password }, { withCredentials: true });
+        console.log(`${API_URL}admin/login`, email, password);
+
+        const response = await axios.post(
+            `${API_URL}admin/login`,
+            { email, password },
+            { withCredentials: true }
+        );
+
         const token = response.data.data;
-
-        // Split the JWT
-        const parts = token.split('.');
-
-        // The payload is the second part
+        localStorage.setItem("token", token);
+        const parts = token.split(".");
         const payload = parts[1];
 
-        // Decode base64
         const decodedPayload = JSON.parse(atob(payload));
 
-        console.log('Decoded JWT payload:', decodedPayload);
-        console.log(response.data);
+        console.log("Decoded JWT payload:", decodedPayload);
+        console.log("Login response:", response.data);
+        console.log("Token saved:", localStorage.getItem("token"));
+
         return response.data;
+
     } catch (error) {
-        console.error(error);
+        console.error("Login error:", error);
+        throw error;
     }
-}
+};
 
 export const requestPasswordReset = async (email: string) => {
     try {
